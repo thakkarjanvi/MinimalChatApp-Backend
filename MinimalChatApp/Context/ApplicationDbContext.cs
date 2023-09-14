b5745fc7383cf86768b9c76b3914c7e37c54d7ca
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-09-14 17:57:21 +0530

    Project Setup

---
+++ MinimalChatApp/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinimalChatApp.Context
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
