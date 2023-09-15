f518d98618ca7fbfc7e3982cf7e6c0652fc4d5ca
thakkarjanvi <janvithakkar18122001@gmail.com>
2023-09-15 17:21:03 +0530

    Implemented Login Successfully

--- MinimalChatApp/Program.cs
+++ MinimalChatApp/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MinimalChatApp.Context;
 using MinimalChatApp.Models;
+using System.Text;
 
 namespace MinimalChatApp
 {
@@ -24,6 +27,21 @@ namespace MinimalChatApp
             builder.Services.AddDbContext<ApplicationDbContext>
             (options => options.UseSqlServer(builder.Configuration.GetConnectionString("MinimalChatApp")));
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "your_issuer",
+                        ValidAudience = "your_audience",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key"))
+                    };
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -36,7 +54,7 @@ namespace MinimalChatApp
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
 
             app.MapControllers();
 
